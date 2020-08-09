b0392a8026c382e2e191a53f131dc8752ee3ccce
Lance McCarthy <lance.mccarthy@progress.com>
2020-08-09 19:33:26 -0400

    General cleanup and linting

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,22 +2,12 @@ import * as main from '../src/main'
 
 test('validate connection_string', async () => {
   await expect(
-    main.uploadToAzure(
-      "",
-      "",
-      "src/abcd",
-      "",
-      false,
-      false)).rejects.toThrow('The connection_string cannot be empty.')
+    main.uploadToAzure('', '', 'src/abcd', '', false, false)
+  ).rejects.toThrow('The connection_string cannot be empty.')
 })
 
 test('validate source_folder', async () => {
   await expect(
-    main.uploadToAzure(
-      "XXXXXXXXXXXXXXXXXXXXXXXXXXX",
-      "",
-      "",
-      "",
-      false,
-      false)).rejects.toThrow('The source_folder was not a valid value.')
+    main.uploadToAzure('XXXXXXXXXXXXXXXXXXXXXXXXXXX', '', '', '', false, false)
+  ).rejects.toThrow('The source_folder was not a valid value.')
 })
