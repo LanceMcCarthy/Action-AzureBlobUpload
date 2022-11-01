6c0c4a36b7b81238270a6addad22361be6b6bdcf
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-11-01 16:22:20 -0400

    Update methods-azure.test.ts
    Added test for missing container name error

--- __tests__/methods-azure.test.ts
+++ __tests__/methods-azure.test.ts
@@ -3,10 +3,20 @@ import {expect, describe, test, it} from '@jest/globals';
 
 describe('UploadToAzure', () => {
   it('Should fail if no connection string was provided', async () => {
-    await expect(azure.UploadToAzure('', '', 'src/abcd', '', false, false, true, false)).rejects.toThrow('The connection_string cannot be empty.');
+    await expect(azure.UploadToAzure('', 'container-name', 'src/abcd', '', false, false, true, false)).rejects.toThrow(
+      'The connection_string cannot be empty.'
+    );
   });
 
   it('Should fail if no source folder was provided', async () => {
-    await expect(azure.UploadToAzure('XXXXXXXX', '', '', '', false, false, true, false)).rejects.toThrow('The source_folder was not a valid value.');
+    await expect(azure.UploadToAzure('connection-string', 'container-name', '', '', false, false, true, false)).rejects.toThrow(
+      'The source_folder was not a valid value.'
+    );
+  });
+
+  it('Should fail if no container name was provided', async () => {
+    await expect(azure.UploadToAzure('connection-string', '', 'src/abcd', '', false, false, true, false)).rejects.toThrow(
+      'The container_name cannot be empty.'
+    );
   });
 });
