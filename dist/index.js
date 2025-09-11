caa002d5a92f7436062e44f38031c0c3ff17011e
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-09-11 17:44:53 -0400

    Build v3.5.0

--- dist/index.js
+++ dist/index.js
@@ -43,25 +43,6 @@ Object.defineProperty(exports, "__esModule", ({ value: true }));
 const core = __importStar(__nccwpck_require__(37484));
 const methods_azure_1 = __nccwpck_require__(37940);
 async function run() {
-    // Parameters from the developer in their GitHub Actions workflow
-    const connectionString = core.getInput('connection_string');
-    const tenantId = core.getInput('tenant_id');
-    const clientId = core.getInput('client_id');
-    const clientSecret = core.getInput('client_secret');
-    const storageAccount = core.getInput('storage_account');
-    core.debug('Parameters received:');
-    core.debug(`connection_string: ${connectionString ? '***' : '<not provided>'}`);
-    core.debug(`tenant_id: ${tenantId ? '***' : '<not provided>'}`);
-    core.debug(`client_id: ${clientId ? '***' : '<not provided>'}`);
-    core.debug(`client_secret: ${clientSecret ? '***' : '<not provided>'}`);
-    core.debug(`storage_account: ${storageAccount ? '***' : '<not provided>'}`);
-    let authPayload;
-    if (connectionString) {
-        authPayload = { type: 'connection_string', connectionString };
-    }
-    else {
-        authPayload = { type: 'service_principal', tenantId, clientId, clientSecret, storageAccount };
-    }
     const containerName = core.getInput('container_name');
     const sourceFolder = core.getInput('source_folder');
     const destinationFolder = core.getInput('destination_folder');
@@ -69,6 +50,36 @@ async function run() {
     const failIfSourceEmpty = core.getInput('fail_if_source_empty').toLowerCase() === 'true';
     const isRecursive = core.getInput('is_recursive').toLowerCase() === 'true';
     const deleteIfExists = core.getInput('delete_if_exists').toLowerCase() === 'false';
+    // AuthType 1 - If using ConnectionString, this value is required
+    // Note if a value is present, we automatically switch into AuthType 1 (see methods-azure.ts Line 59)
+    const connectionString = core.getInput('connection_string');
+    // AuthType 2 - If using Service Principal, these values are required
+    const tenantId = core.getInput('tenant_id');
+    const clientId = core.getInput('client_id');
+    const clientSecret = core.getInput('client_secret');
+    const storageAccount = core.getInput('storage_account');
+    // For debugging purposes only
+    // When forking this repo, be careful to not log secrets actual values (Github secrets should be masked, but be safe by default).
+    // core.debug('Parameters received:');
+    // core.debug(`container_name: ${containerName ? '***' : '<not provided>'}`);
+    // core.debug(`source_folder: ${sourceFolder ? '***' : '<not provided>'}`);
+    // core.debug(`destination_folder: ${destinationFolder ? '***' : '<not provided>'}`);
+    // core.debug(`clean_destination_folder: ${cleanDestinationPath ? '***' : '<not provided>'}`);
+    // core.debug(`fail_if_source_empty: ${failIfSourceEmpty ? '***' : '<not provided>'}`);
+    // core.debug(`is_recursive: ${isRecursive ? '***' : '<not provided>'}`);
+    // core.debug(`delete_if_exists: ${deleteIfExists ? '***' : '<not provided>'}`);
+    // core.debug(`connection_string: ${connectionString ? '***' : '<not provided>'}`);
+    // core.debug(`tenant_id: ${tenantId ? '***' : '<not provided>'}`);
+    // core.debug(`client_id: ${clientId ? '***' : '<not provided>'}`);
+    // core.debug(`client_secret: ${clientSecret ? '***' : '<not provided>'}`);
+    // core.debug(`storage_account: ${storageAccount ? '***' : '<not provided>'}`);
+    let authPayload;
+    if (connectionString) {
+        authPayload = { type: 'connection_string', connectionString };
+    }
+    else {
+        authPayload = { type: 'service_principal', tenantId, clientId, clientSecret, storageAccount };
+    }
     // invoke this Action's main entry method
     await (0, methods_azure_1.UploadToAzure)({
         authPayload,
