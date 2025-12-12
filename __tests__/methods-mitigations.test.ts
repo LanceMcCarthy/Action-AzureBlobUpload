934b7679ba88388251ac1822049d6d28df863e5a
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-12-12 14:14:26 -0500

    Update methods-mitigations.test.ts
    Added test to add 100% coverage here

--- __tests__/methods-mitigations.test.ts
+++ __tests__/methods-mitigations.test.ts
@@ -14,3 +14,16 @@ describe('getFinalPathForFileName', () => {
     expect(mitigations.checkForFirstDuplicateInPath(brokenPath)).toEqual(fixedPath);
   });
 });
+
+describe('checkForMultipleDuplicatesInPath', () => {
+  it('Should remove every repeated segment from the path', () => {
+    const brokenPath = 'D:/a/Action-AzureBlobUpload/Action-AzureBlobUpload/TestFiles/ExcelFiles/ExcelFiles/Test.xlsx';
+    const fixedPath = 'D:/a/Action-AzureBlobUpload/TestFiles/ExcelFiles/Test.xlsx';
+    expect(mitigations.checkForMultipleDuplicatesInPath(brokenPath)).toEqual(fixedPath);
+  });
+
+  it('Should return the original path when no duplicates exist', () => {
+    const validPath = 'D:/a/Action-AzureBlobUpload/TestFiles/ExcelFiles/Test.xlsx';
+    expect(mitigations.checkForMultipleDuplicatesInPath(validPath)).toEqual(validPath);
+  });
+});
