4859c7396a1d6593fe8d922782d39b7801009526
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2026-02-02 10:01:37 -0500

    Fix testing issues

---
+++ __mocks__/actions-core.ts
@@ -0,0 +1,11 @@
+import {jest} from '@jest/globals';
+
+export const info = jest.fn();
+export const error = jest.fn();
+export const debug = jest.fn();
+export const warning = jest.fn();
+export const setFailed = jest.fn();
+export const getInput = jest.fn();
+export const setOutput = jest.fn();
+export const exportVariable = jest.fn();
+export const addPath = jest.fn();
--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@ module.exports = {
   clearMocks: true,
   moduleFileExtensions: ['js', 'ts'],
   testMatch: ['**/*.test.ts'],
+  moduleNameMapper: {
+    '^@actions/core$': '<rootDir>/__mocks__/actions-core.ts'
+  },
   transform: {
     '^.+\\.ts$': 'ts-jest'
   }
